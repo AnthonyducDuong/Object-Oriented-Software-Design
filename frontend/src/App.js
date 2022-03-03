fba21949d0026d6c4eafaf512fb89a2ddac5a53f
Phan Thanh Tài <teo2001tn@gmail.com>
2022-03-03 15:37:31 +0700

    completed footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,10 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Header from './components/header';
 import TopBar from './components/header/TopBar';
 import TopNav from './components/header/TopNav';
-import Footer from './components/Footer';
-
 function App() {
   return (
     <ChakraProvider>
       <Header />
-      <Footer />
     </ChakraProvider>
   );
 }
